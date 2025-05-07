6c6c24c6fa05170b58792296771f0bcd9d52a0fb
flaviare1s <flaviarster@gmail.com>
2025-05-07 16:13:59 -0300

    feat: adiciona seção de listagem de pets na home

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { Login } from "./pages/Login"
 import { UserRegister } from "./pages/UserRegister"
 import { GuardianRegister } from "./pages/GuardianRegister"
 import { Pets } from "./pages/Pets"
-import { PetCard } from "./pages/PetCard"
 import { PetRegister } from "./pages/PetRegister"
 import { PetEdit } from "./pages/PetEdit"
 import { NotFound } from "./pages/NotFound"
@@ -14,6 +13,7 @@ import { Congratulations } from "./pages/Congratulations"
 import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
 import { SelectAccountType } from "./pages/SelectAccountType"
+import { PetProfile } from "./pages/PetProfile"
 
 function App() {
 
@@ -30,7 +30,7 @@ function App() {
             <Route path="/signup/user" element={<UserRegister />} />
             <Route path="/signup/guardian" element={<GuardianRegister />} />
             <Route path="/pets" element={<Pets />} />
-            <Route path="/pets/:id" element={<PetCard />} />
+            <Route path="/pets/:id" element={<PetProfile />} />
             <Route path="/pets/add" element={<PetRegister />} />
             <Route path="/pets/edit/:id" element={<PetEdit />} />
             <Route path='/ongs' element={<ONGs />} />
--- src/pages/PetCard.jsx
+++
@@ -1,7 +0,0 @@
-export const PetCard = () => {
-  return (
-    <div>
-      Pet
-    </div>
-  )
-}

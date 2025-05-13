33d909b53e368de4f55dd459be1d092cb7a22db1
flaviare1s <flaviarster@gmail.com>
2025-05-13 16:02:04 -0300

    style: ajusta estilo da página de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,6 +6,11 @@ import { useState, useEffect } from "react";
 import toast from "react-hot-toast";
 import { fetchUsers } from "../api/auth";
 
+import bgDog1 from "../assets/bg-dog1.png"
+import bgDog2 from "../assets/bg-dog2.png"
+import bgDog3 from "../assets/bg-dog3.png"
+import bgDog4 from "../assets/bg-dog4.png"
+
 export const Login = () => {
   const navigate = useNavigate();
   // eslint-disable-next-line no-unused-vars
@@ -51,7 +56,19 @@ export const Login = () => {
   }, []);
 
   return (
-    <div className="bg-rosa-forte flex flex-col flex-grow min-h-[95vh] items-center justify-center">
+    <div className="bg-rosa-forte flex flex-col flex-grow min-h-[95vh] items-center justify-center relative">
+      <div className="absolute bottom-0 right-0 hidden lg:block w-[300px]">
+        <img className="w-full" src={bgDog1} alt="Desenho de cachorro" />
+      </div>
+      <div className="absolute bottom-0 left-0 hidden lg:block w-[300px]">
+        <img className="w-full" src={bgDog2} alt="Desenho de cachorro" />
+      </div>
+      <div className="absolute bottom-[300px] right-0 hidden lg:block w-[300px]">
+        <img className="w-full" src={bgDog3} alt="Desenho de cachorro" />
+      </div>
+      <div className="absolute bottom-[300px] left-0 hidden lg:block w-[300px]">
+        <img className="w-full" src={bgDog4} alt="Desenho de cachorro" />
+      </div>
       <div className="bg-white p-[30px] flex flex-col justify-center w-[90%] sm:w-[500px] rounded-xs">
         <h2 className="font-bold text-xl text-center mb-2 sm:text-2xl">
           <span className="text-verde-primario">&lt;</span>Faça seu Login

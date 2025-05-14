c6d6b94ab4e2faf13de43e81662b347d9d739617
Flávia Reis <flaviarster@gmail.com>
2025-05-14 08:50:42 -0300

    fix: corrige rotas privadas

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,16 @@
-import { Route, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useUser } from "../contexts/userContext";
 
 export const PrivateRoute = ({ children }) => {
   const { user } = useUser();
 
-  if (!user || user.role !== "guardian") {
+  if (!user) {
+    return <Navigate to="/login" />
+  }
 
-    return <Navigate to="/" replace />;
+  if (user.role !== "guardian") {
+
+    return <Navigate to="/forbidden" replace />;
   }
 
   return children;

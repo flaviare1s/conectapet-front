820481860c14f753a5e6d7be4d1aa4388d734ede
palloma <palloma.ufpb.ri@gmail.com>
2025-05-15 13:23:05 -0300

    fix:ongsection

--- src/components/ONGsSectionItem.jsx
+++ src/components/ONGsSectionItem.jsx
@@ -1,9 +1,17 @@
-export const ONGsSectionItem = ({ image }) => {
+const borderClasses = {
+  "verde-primario": "border-2 border-verde-primario",
+  "roxo-primario": "border-2 border-roxo-primario",
+};
+
+
+export const ONGsSectionItem = ({ image, name, borderColor }) => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="bg-white p-2 rounded-full shadow">
-        <div className="w-[56px] md:w-[83px] aspect-square :text-Primary">{image}</div>
+      <div className={`bg-white p-1 rounded-full shadow border border-purple-200 ${borderClasses[borderColor]}`}>
+        <img className="w-[56px] md:w-[83px] aspect-square: text-Primary rounded-full" src={image} alt= {`Logo da ONG {name}`}/>
       </div>
+      <p className="font-semibold text-xs p-2"> {name} </p>
+
     </div>
   )
 }

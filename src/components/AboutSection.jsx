03e9803b35ebb658cfb774195929e50ba7b54134
Olavo Vieira <olavovieira203@gmail.com>
2025-05-16 16:30:03 -0300

    feat: about em desenvolvimento

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+import Dog from '../assets/about/blackdog-mobile.png'
+
 export const AboutSection = () => {
   return (
-    <div>AboutSection</div>
+    <div className="bg-rosa-forte md:w-376px md:h-740px">
+      <div>
+        <h2 className="text-verde-vibrante">Sobre nós</h2>
+        <h1 className="text-branco">ConectaPet</h1>
+        <p className="text-branco">Somos uma ponte entre animais resgatados e pessoas dispostas a amar. No ConectaPet, você encontra pets que precisam de um lar seguro, afetuoso e responsável. Juntos, podemos transformar histórias e espalhar mais amor pelo mundo – um pet de cada vez.</p>
+      </div>
+      <div className='bg-verde-primario'>
+        <Link to="/about" className='text-preto'>Conheça Nossa Missão</Link>
+      </div>
+      <div>
+        <img src={Dog} alt="Dog Black" />
+      </div>
+    </div>
   )
 }

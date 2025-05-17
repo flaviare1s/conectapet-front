827d0602f4676be85176d75843b4dc03a1f1a6b7
flaviare1s <flaviarster@gmail.com>
2025-05-17 09:29:53 -0300

    feat: adiciona card de pet por responsável

--- src/components/PetsListByGuardian.jsx
+++ src/components/PetsListByGuardian.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-import { PetCard } from "./PetCard";
 import { useEffect, useState } from "react";
 import { getPetsByGuardianId } from "../api/pets";
 import { Loader } from "./Loader";
+import { PetCardByGuardian } from "./PetCardByGuardian";
 
-export const PetsListByGuardian = ({ guardianId, showDescricao = true }) => {
+export const PetsListByGuardian = ({ guardianId }) => {
   const [pets, setPets] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -36,7 +36,7 @@ export const PetsListByGuardian = ({ guardianId, showDescricao = true }) => {
       <div className="flex items-center justify-between mb-5">
         <h2 className="font-bold text-sm sm:text-base md:text-xl">Galeria dos peludinhos:</h2>
         <nav className="flex items-center gap-6">
-          <Link className="bg-roxo-primario text-white hover:bg-rosa-forte rounded-md py-2 px-2 sm:px-6 text-xs font-bold hover:underline" to="/pets/add">Novo pet</Link>
+          <Link className="bg-roxo-primario text-white hover:bg-rosa-forte rounded-md py-2 px-2 sm:px-6 text-xs font-bold" to="/pets/add">Novo pet</Link>
           <Link className="text-roxo-primario hover:underline" to="/">Voltar</Link>
         </nav>
       </div>
@@ -44,7 +44,7 @@ export const PetsListByGuardian = ({ guardianId, showDescricao = true }) => {
         {pets.length === 0 && <p className="text-gray-600 h-[60vh]">Nenhum pet cadastrado.</p>}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {pets.map((pet) => (
-            <PetCard key={pet.id} pet={pet} showDescricao={showDescricao} />
+            <PetCardByGuardian key={pet.id} pet={pet} />
           ))}
         </div>
       </div>

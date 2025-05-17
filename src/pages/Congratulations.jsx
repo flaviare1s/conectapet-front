4371ef7746550d281c04ba2658a9e17b1449386b
flaviare1s <flaviarster@gmail.com>
2025-05-17 15:12:14 -0300

    fix: ajusta links nos formulários de registro e login

--- src/pages/Congratulations.jsx
+++ src/pages/Congratulations.jsx
@@ -1,7 +1,7 @@
 export const Congratulations = () => {
   return (
-    <div>
-      Congratulations
+    <div className="flex flex-col flex-grow items-center justify-center">
+      <p className="text-2xl max-w-1/2 text-center">Deu match, mas essa página ainda está em construção!</p>
     </div>
   )
 }

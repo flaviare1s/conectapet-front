a3e3d37784e13d442693b43dccf9a87e53220bfc
flaviare1s <flaviarster@gmail.com>
2025-05-18 08:46:38 -0300

    atyle: ajusta padding

--- src/pages/PetProfile.jsx
+++ src/pages/PetProfile.jsx
@@ -33,7 +33,7 @@ export const PetProfile = () => {
 
   return (
     <section>
-      <div className="flex items-center justify-between mb-6 max-w-4xl mx-auto">
+      <div className="flex items-center justify-between mb-6 max-w-4xl mx-auto px-6">
         <h2 className="text-xl font-semibold text-roxo-primario">
           <span className="text-verde-primario">&lt;</span>
           Meu Perfil

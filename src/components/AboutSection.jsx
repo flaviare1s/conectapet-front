d67f43a53d4162e54d3f4c0bcc1d626536b7d90c
flaviare1s <flaviarster@gmail.com>
2025-05-18 06:57:03 -0300

    style: ajusta seção about

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,24 +1,21 @@
 import { Link } from 'react-router-dom';
 import Dog from '../assets/about/blackdog-mobile.png'
-import DogDesktop from '../assets/about/blackdog-desktop.png'
-
 export const AboutSection = () => {
   return (
-    <div className="bg-rosa-forte flex flex-col md:flex-row-reverse justify-center items-center p-6 md:p-10 lg:px-20 gap-15">
-      <div className='max-w-[500px]'>
-        <h2 className="text-verde-vibrante font-semibold">Sobre nós:</h2>
-        <h1 className="text-branco text-3x1 py-2">ConectaPet</h1>
-        <div className='items-center justify-center flex flex-col'>
+    <div className="bg-rosa-forte flex flex-col lg:flex-row-reverse justify-center items-center lg:gap-15 lg:pt-10">
+      <div className='max-w-[500px] flex flex-col p-5'>
+        <h2 className="text-verde-vibrante font-semibold text-lg">Sobre nós:</h2>
+        <h1 className="text-branco text-2xl sm:text-4xl py-2 font-bold">ConectaPet</h1>
+        <div className='flex flex-col justify-center'>
           <p className="text-branco">Somos uma ponte entre animais resgatados e pessoas dispostas a amar. No ConectaPet, você encontra pets que precisam de um lar seguro, afetuoso e responsável. Juntos, podemos transformar histórias e espalhar mais amor pelo mundo – um pet de cada vez.</p>
-          <div className='bg-verde-primario hover:bg-verde-escuro w-[230px] h-[40px] flex items-center justify-center text-sm font-medium rounded-md mt-4'>
+          <div className='bg-verde-primario hover:bg-verde-escuro w-[230px] h-[40px] flex items-center justify-center text-sm font-medium rounded-md mt-5'>
             <Link to="/about" className='text-preto'>Conheça Nossa Missão</Link>
           </div>
         </div>
       </div>
-      <div className='mt-20 mb-0 relative flex items-center justify-center'>
+      <div className='mt-5 mb-0 relative flex items-center justify-center'>
         <picture>
-          <source srcSet={DogDesktop} media='(min-width: 1000px)' />
-          <img src={Dog} alt="Dog Black" className='mb-0 relative z-10'/>
+          <img src={Dog} alt="Dog Black" className='w-[300px] sm:w-[400px]'/>
         </picture>
       </div>
     </div>

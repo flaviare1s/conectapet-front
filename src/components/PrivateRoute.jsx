cab1956c0d6d01e487a7065bf7a19571936cc6c2
flaviare1s <flaviarster@gmail.com>
2025-05-18 08:24:42 -0300

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,17 +1,17 @@
-// import { Navigate } from "react-router-dom";
-// import { useAuth } from "../contexts/AuthConText";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthConText";
 
-// export const PrivateRoute = ({ children }) => {
-//   const { user } = useAuth();
+export const PrivateRoute = ({ children }) => {
+  const { user } = useAuth();
 
-//   if (!user) {
-//     return <Navigate to="/login" />
-//   }
+  if (!user) {
+    return <Navigate to="/login" />
+  }
 
-//   if (user.role !== "guardian") {
+  if (user.role !== "guardian") {
 
-//     return <Navigate to="/forbidden" replace />;
-//   }
+    return <Navigate to="/forbidden" replace />;
+  }
 
-//   return children;
-// };
+  return children;
+};

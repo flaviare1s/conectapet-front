126ebff7f86e2d68b0b9fb55698c72886dbc6005
flaviare1s <flaviarster@gmail.com>
2025-05-19 16:51:34 -0300

    feat: add loader em MyPets

--- src/pages/MyPets.jsx
+++ src/pages/MyPets.jsx
@@ -1,12 +1,17 @@
+import { Loader } from "../components/Loader";
 import { PetsListByGuardian } from "../components/PetsListByGuardian";
 import { useUser } from "../contexts/UserContext";
 
 export const MyPets = () => {
   const { user } = useUser();
-  
+
+  if (!user) {
+    return <Loader />;
+  }
+
   return (
     <div>
-      <PetsListByGuardian guardianId={user?.id} />
+      <PetsListByGuardian guardianId={user.id} />
     </div>
   );
 };

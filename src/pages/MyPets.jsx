609fa335e75a0fa8ed40f7237be7e4f4ea093343
flaviare1s <flaviarster@gmail.com>
2025-05-19 15:16:05 -0300

    fix: ajusta cadastro, edição e filtragem de pets

--- src/pages/MyPets.jsx
+++ src/pages/MyPets.jsx
@@ -3,11 +3,7 @@ import { useUser } from "../contexts/UserContext";
 
 export const MyPets = () => {
   const { user } = useUser();
-
-  if (!user || user.role !== "guardian") {
-    return <p className="text-center mt-10 text-red-500">Acesso não autorizado.</p>;
-  }
-
+  
   return (
     <div>
       <PetsListByGuardian guardianId={user?.id} />

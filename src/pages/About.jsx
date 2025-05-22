07bcfec73db5fc39638977212aa6fc3976a0ed55
pallomadvm <palloma.ufpb.ri@gmail.com>
2025-05-22 17:22:03 -0300

    fix/about/arquivo-env-example

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,34 +11,34 @@ export const About = () => {
       </div>
         <div className="flex flex-col flex-grow items-center justify-center">
           <h1 className="text-verde-primario text-center text-2xl md:text-5xl p-4 font-bold">Nossa missão</h1>
-          <p className="md:text-2xl text-left px-10 pb-5">
+          <p className="md:text-2xl text-justify px-10 pb-5">
             No <strong className="md:text-3xl text-roxo-primario">ConectaPet</strong>, acreditamos que todo animal merece amor, cuidado e um lar seguro. Nossa missão é ser a ponte entre animais resgatados e pessoas dispostas a oferecer uma nova chance para quem já sofreu demais. Mais do que um site, somos um movimento de empatia, responsabilidade e esperança.
           </p>
-          <p className="md:text-2xl text-left px-10 pb-5">
+          <p className="md:text-2xl text-justify px-10 pb-5">
             Sabemos que cada pet tem uma história única — alguns foram abandonados, outros resgatados de situações difíceis. Mas todos têm em comum o desejo por algo simples: um lar onde possam se sentir protegidos, valorizados e amados. É por isso que trabalhamos todos os dias para conectaresses animais a pessoas que queiram fazer a diferença.
           </p>
-          <p className="md:text-2xl text-left px-10 pb-5">
+          <p className="md:text-2xl text-justify px-10 pb-5">
             Ao adotar um pet pelo ConectaPet, você não está apenas ganhando um amigo fiel. Está escrevendo um novo capítulo na vida de um ser que só precisava de uma chance. E, ao mesmo tempo, está transformando sua própria história com mais afeto, companheirismo e alegria.
           </p>
-          <p className="md:text-2xl text-left px-10 pb-5">
+          <p className="md:text-2xl text-justify px-10 pb-5">
             Junte-se a nós nessa jornada de transformação. Um pet adotado pode não mudar o mundo inteiro,mas com certeza muda o mundo de alguém — inclusive o seu.
           </p>
         </div>
         {/* Cadastro */}
         <div className="w-full max-w-2xl flex items-center justify-center flex-col">
-          <h2 className="text-center text-verde-primario md:text-4xl p-5 font-bold">Passo a passo cadastro🐾</h2>
+          <h2 className="text-center text-verde-primario md:text-4xl p-5 font-bold">Passo a passo cadastro 🐾</h2>
           <h3 className="text-left md:text-3xl py-4 text-roxo-primario font-bold">Quero Adotar</h3>
           <ul className="text-left md:text-xl">
             <li className="mb-10">
               <div className="flex items-center gap-2">
                 <VscAccount className="text-verde-primario text-5xl pl-3"/>
-                <h4 className="font-bold">Crie um Cadastro</h4>
+                <h4 className="font-bold">Crie um cadastro</h4>
               </div>
               <ul className="list-disc list-outside px-10 text-left md:text-xl mx-auto">
-                <li>Na barra de navegação escolha a opção "Cadastre-se"</li>
-                <li>Escolha o tipo de cadastro "Quero Adotar"</li>
-                <li>Preencha com Nome, Email e Senha com no minimo 6 digitos</li>
-                <li>CADASTRO REALIZADO COM SUCESSO</li>
+                <li>Na barra de navegação, escolha a opção "Cadastre-se".</li>
+                <li>Escolha o tipo de cadastro "Quero Adotar".</li>
+                <li>Preencha os campos nome, email e senha (com no mínimo 6 dígitos).</li>
+
               </ul>
             </li>
             <li className="mb-10">
@@ -47,10 +47,10 @@ export const About = () => {
                 <h4 className="font-bold">Galeria de Pets</h4>
               </div>
               <ul className="list-disc list-outside px-10 text-left md:text-xl mx-auto">
-                <li>Na barra de navegação escolha a opção "Pets".</li>
-                <li>Na página Pets vai ter varias opções para adoção (Cuidado, no canto superior esquerdo tem o status de adoção).</li>
-                <li>Ao escolher o pet que mais se apaixonou, vai ser direcionado para o perfil do pet.</li>
-                <li>No perfil do pet você, vai conhecer melhor seu novo amiguinho.</li>
+                <li>Na barra de navegação, escolha a opção "Pets".</li>
+                <li>Na página "Pets", você encontrará várias opções de animais disponíveis para adoção. Fique atento ao status de adoção, exibido no canto superior esquerdo de cada card.</li>
+                <li>Ao escolher o pet que mais gostar, você será direcionado para o perfil dele.</li>
+                <li>No perfil do pet, você vai conhecer melhor seu novo amiguinho.</li>
               </ul>
             </li>
             <li className="mb-10">
@@ -58,7 +58,7 @@ export const About = () => {
                 <SiGoogleforms  className="text-verde-primario text-5xl pl-3"/>
                 <h4 className="font-bold">Formulário</h4>
               </div>
-              <p className="p-10 py-5">
+              <p className="p-10 py-5 text-justify">
                 Nosso formulário foi desenvolvido para tornar o processo de adoção simples, rápido e seguro. Ao preencher seus dados corretamente, você nos ajuda a garantir que cada pet encontre o lar ideal. É através desse formulário que conectamos amor, responsabilidade e novas histórias. Preencha com carinho — cada informação faz a diferença na vida de um animal resgatado. 🐾
               </p>
             </li>

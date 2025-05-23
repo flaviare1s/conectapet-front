0732b34bae85d339759def6e4b4a3bda63fc9d4a
flaviare1s <flaviarster@gmail.com>
2025-05-23 05:49:22 -0300

    fix: ajusta link

--- src/pages/Congratulations.jsx
+++ src/pages/Congratulations.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom"
 import { SubmitButton } from "../components/SubmitButton"
 import { FaHeart } from "react-icons/fa6"
 export const Congratulations = () => {
   return (
     <>
-        <div className="display: flex justify-center bg-gray-100 h-300">
-        <div className="max-w-200 m-1 sm:m-25 bg-white">
+        <div className="display: flex justify-center bg-gray-100">
+        <div className="max-w-200 m-1 sm:m-25 bg-white rounded-md">
           <div className="display: flex justify-center text-6xl text-roxo-primario pb-5 pt-8"><FaHeart/></div>
         
           <div className="display: flex justify-center text-3xl font-bold text-center text-roxo-primario font-inter: font-sans-serif w-50 m-auto pb-5">   
@@ -35,10 +36,8 @@ export const Congratulations = () => {
             <span>Telefone:</span>            
           </div>
 
-          <div className="m-4">
-            <SubmitButton
-              label='Voltar para home'
-            />
+          <div className="w-full flex items-center justify-center p-6">
+            <Link className="text-white text-center font-medium text-sm py-2 px-4 rounded-md transition-colors duration-75 font-inter focus:outline-none focus:shadow-outline w-full cursor-pointer mt-5 bg-verde-primario hover:bg-verde-escuro" to="/">Voltar para a home</Link>
           </div>
       </div>
       </div>

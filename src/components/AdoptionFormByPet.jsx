6510d92f833e0a70163693b3a285e3d15c7e0c51
flaviare1s <flaviarster@gmail.com>
2025-06-03 05:13:20 -0300

    style: ajusta adoption form

--- src/components/AdoptionFormByPet.jsx
+++ src/components/AdoptionFormByPet.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { FaPaw } from "react-icons/fa";
+import { IoIosStar, IoIosStarOutline } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 export const AdoptionFormByPet = ({ adoption }) => {
+  const [favorited, setFavorited] = useState(false);
+
+  const handleFavorite = () => {
+    setFavorited(!favorited);
+  };
+
   return (
     <div className="bg-verde-piscina/20 border border-verde-escuro/30 rounded-2xl shadow-lg p-6 hover:scale-105 transition-transform">
-      <div className="flex items-center gap-2 mb-4">
-        <FaPaw className="text-verde-primario text-2xl" />
-        <h3 className="text-lg font-bold text-roxo-primario">
-          Deu match com {adoption.petName}!
-        </h3>
+      <div className="flex justify-between items-center mb-4">
+        <div className="flex items-center gap-2">
+          <FaPaw className="text-verde-primario text-2xl" />
+          <h3 className="text-lg font-bold text-roxo-primario">
+            Deu match com {adoption.petName}!
+          </h3>
+        </div>
+        <button className="text-roxo-primario text-2xl cursor-pointer"><IoClose /></button>
       </div>
       <div className="space-y-1 text-cinza">
         <p>
@@ -37,6 +49,9 @@ export const AdoptionFormByPet = ({ adoption }) => {
           {adoption.motivacao}
         </p>
       </div>
+      <button onClick={handleFavorite} className="mt-3 text-2xl cursor-pointer">
+        {favorited ? <IoIosStar className="text-roxo-primario" /> : <IoIosStarOutline className="text-gray-400" />}
+      </button>
     </div>
   );
 };

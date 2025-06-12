6ee9f7437c8f3c68359ea508ffd01b9a32c84674
flaviare1s <flaviarster@gmail.com>
2025-06-12 06:21:24 -0300

    test: finaliza crud de pets (cenário feliz)

---
+++ cypress/e2e/pets/5-listByGuardianId.cy.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/* ==== Test Created with Cypress Studio ==== */
+it('Deve listar todos os pets de um guardian específico', function() {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit('http://localhost:5173/mypets');
+  cy.get('#email').clear('apata@teste.com');
+  cy.get('#email').type('apata@teste.com');
+  cy.get('#senha').clear();
+  cy.get('#senha').type('123456');
+  cy.get('.my-4 > .text-white').click();
+  cy.wait(2000);
+  /* ==== End Cypress Studio ==== */
+});
---
+++ cypress/e2e/pets/6-getAll.cy.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+/* ==== Test Created with Cypress Studio ==== */
+it('Deve listar todos os pets', function() {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit('http://localhost:5173/pets');
+  /* ==== End Cypress Studio ==== */
+});

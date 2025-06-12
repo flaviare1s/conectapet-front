a87fde4f0b1c315aaf6c89741c0bff3139a04bf6
flaviare1s <flaviarster@gmail.com>
2025-06-12 05:44:13 -0300

    test: ajusta post e update de pets

--- cypress/e2e/pets/update.cy.js
+++ cypress/e2e/pets/update.cy.js
@@ -2,14 +2,14 @@
 /* ==== Test Created with Cypress Studio ==== */
 it('Deve atualizar um pet corretamente, logado como guardian', function() {
   /* ==== Generated with Cypress Studio ==== */
-  cy.visit('http://localhost:5173/mypets/edit/52');
+  cy.visit('http://localhost:5173/mypets/edit/22');
   cy.get('#email').clear();
-  cy.get('#email').type('apata@teste.com');
+  cy.get('#email').type('gpa@teste.com');
   cy.get('#senha').clear();
   cy.get('#senha').type('123456');
   cy.get('.my-4 > .text-white').click();
-  cy.get('[href="/mypets/edit/52"] > .text-verde-primario').click();
-  cy.get('#nome').clear('Lolinha');
+  cy.get('[href="/mypets/edit/22"] > .text-verde-primario').click();
+  cy.get('#nome').clear('Lol');
   cy.get('#nome').type('Lola');
   cy.get('.my-4 > .text-white').click();
   cy.wait(2000);

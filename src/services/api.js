2f7c91ade36f103a25298a1312c8b91a4dccb9e2
flaviare1s <flaviarster@gmail.com>
2025-06-16 15:54:25 -0300

    feat: refresh token

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,47 @@ const api = axios.create({
   baseURL: "http://localhost:3000",
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+
+        const response = await axios.post("http://localhost:3000/login/refresh-token", {
+          refreshToken,
+        });
+
+        const { token: newToken } = response.data;
+        localStorage.setItem("token", newToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newToken}`;
+        return api(originalRequest);
+      } catch (refreshError) {
+        console.error("Erro ao renovar token", refreshError);
+        localStorage.removeItem("token");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default api;

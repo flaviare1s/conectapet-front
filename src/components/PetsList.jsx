10fcdebc88bdfcd14bd757302da5bedc41f357fe
flaviare1s <flaviarster@gmail.com>
2025-06-16 09:27:00 -0300

    test: finaliza testes de pet e gera coverage

--- src/components/PetsList.jsx
+++ src/components/PetsList.jsx
@@ -45,7 +45,7 @@ export const PetsList = ({ limit, showDescricao = true, filters = {} }) => {
       <div className="flex flex-col justify-center items-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {petsToShow.length === 0 ? (
-            <p className="text-gray-600">Nenhum resultado encontrado!</p>
+            <p className="text-red-600 text-center p-10 w-[80vw]">Nenhum resultado encontrado!</p>
           ) : (
             petsToShow.map((pet) => (
               <PetCard key={pet.id} pet={pet} showDescricao={showDescricao} />

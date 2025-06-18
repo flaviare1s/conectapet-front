14027e1732fef99a0b8436f7bece98a0d0b94e1b
flaviare1s <flaviarster@gmail.com>
2025-06-18 12:49:16 -0300

    Ajusta título do componente ONG Section

--- src/components/ONGsSection.jsx
+++ src/components/ONGsSection.jsx
@@ -7,11 +7,12 @@ import logoAu from '../assets/logos-ongs/logo-au.jpg'
 import logoCausaPet from '../assets/logos-ongs/logo-causapet.jpg'
 import logoGPA from '../assets/logos-ongs/logo-gpa.jpg'
 import logoUpac from '../assets/logos-ongs/logo-upac.jpg'
+import { Link } from "react-router-dom"
 
 export const ONGsSection = () => {
   return (
     <div className="p-6 md:p-10 lg:px-20 bg-purple-50 flex flex-col items-center justify-center">
-      <h2 className="font-bold md:text-xl text-center">Instituições Parceiras:</h2>
+      <Link to="/ongs" className="font-bold md:text-xl text-center">ONGs que Inspiram:</Link>
 
       <div className="w-full overflow-x-auto flex justify-center items-center">
         <div className="flex flex-nowrap gap-5 md:gap-10 p-5">
